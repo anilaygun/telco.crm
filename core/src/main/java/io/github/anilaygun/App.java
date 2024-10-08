5a21bdfea1567bac48bb65e717ff8482b1482c3a
anilaygun <anil.aygun.37@gmail.com>
2024-10-08 17:09:52 +0300

    Feat: core added

---
+++ core/src/main/java/io/github/anilaygun/App.java
@@ -0,0 +1,13 @@
+package io.github.anilaygun;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
