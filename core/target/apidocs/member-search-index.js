e38dbeb3de5994a16405ecf11172f308eb98ee69
anilaygun <anil.aygun.37@gmail.com>
2024-10-08 18:48:53 +0300

    Fix: core mvn cental repo

--- core/target/apidocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"io.github.anilaygun","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"io.github.anilaygun","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
